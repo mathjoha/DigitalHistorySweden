60644d2925367ce1dc13672345ea59ce59f632f9
mathjoha <MathiasJoha@gmail.com>
2024-11-18 17:04:34 +0100

    Prepare for project sorter CI

---
+++ proj_sort.py
@@ -0,0 +1,39 @@
+import yaml
+from pathlib import Path
+
+project_yaml_path = Path(__file__).parent / 'projects.yaml'
+
+def sort_order(project: dict):
+    """Sort project yaml file for consistency.
+
+    year(started): descending
+    (year)end: descending [adds a placeholder if no end is specified]
+    project name: alphabetical
+    project town: alphabetical
+
+    """
+    project['end'] = project['end'] if 'end' in project.keys() else 9999
+
+    if project['end'] < project['year']:
+        raise ValueError('Project {project} ends before it starts')
+
+    return (
+        -project['year'],
+        -project['end'],
+        project['name'],
+        project['region']
+    )
+
+if __name__ == "__main__":
+    with open(project_yaml_path, 'r', encoding='utf8') as f:
+        raw_yaml = f.read()
+
+    projects_dicts = yaml.safe_load(raw_yaml)
+
+    sorted_projects = sorted(projects_dicts, keys=sort_order0)
+
+    sorted_yaml = yaml.safe_dump(sorted_projects)
+
+    if sorted_yamls != raw_yaml:
+        with open(project_yaml_path, 'w', encoding='utf8') as f:
+            f.write(sorted_yaml)
