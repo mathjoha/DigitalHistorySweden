624edffebe45f208536bb92e8e5756f29e8fb89b
mathjoha <MathiasJoha@gmail.com>
2024-11-23 21:01:05 +0100

    Solve UU

--- src/get_url.py
+++ src/get_url.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup as bs
 
 from get_lu import lu_parse
 from get_umu import umu_parse
+from get_uu import uu_parse
 from paths import url_file
 from project_to_yaml import write_project
 
@@ -16,7 +17,9 @@ if __name__ == "__main__":
     for url in load_urls():
         if "portal.research.lu.se" in url:
             project_data = lu_parse(url)
-        if url.startswith("https://www.umu.se/en/research/projects/"):
+        elif url.startswith("https://www.umu.se/en/research/projects/"):
+            project_data = umu_parse(url)
+        elif url.startswith("https://www.uu.se/en/research/research-projects/project"):
             project_data = uu_parse(url)
 
         write_project(project_data)
