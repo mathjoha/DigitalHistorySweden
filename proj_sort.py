1386c7b8bbcc01e906aae4375486c7c6d486e8e3
mathjoha <MathiasJoha@gmail.com>
2024-11-23 14:46:29 +0100

    Set up pre-commit for sorting projects

--- proj_sort.py
+++ proj_sort.py
@@ -1,8 +1,19 @@
-import yaml
-from pathlib import Path
 from collections import Counter
+from pathlib import Path
 
-project_yaml_path = Path(__file__).parent / 'projects.yaml'
+try:
+    import yaml
+except ModuleNotFoundError:
+    import subprocess
+    import sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyyaml"])
+
+    import yaml
+
+
+project_yaml_path = Path(__file__).parent / "projects.yaml"
+
 
 def sort_order(project: dict):
     """Sort project yaml file for consistency.
@@ -13,35 +24,33 @@ def sort_order(project: dict):
     project town: alphabetical
 
     """
-    project['end'] = project['end'] if 'end' in project.keys() else 9999
+    project["end"] = project["end"] if "end" in project.keys() else 9999
 
-    if project['end'] < project['year']:
-        raise ValueError('Project {project} ends before it starts')
+    if project["end"] < project["year"]:
+        raise ValueError("Project {project} ends before it starts")
 
-    return (
-        -project['year'],
-        -project['end'],
-        project['name'],
-        project['region']
-    )
+    return (-project["year"], -project["end"], project["name"], project["town"])
+
 
 if __name__ == "__main__":
-    with open(project_yaml_path, 'r', encoding='utf8') as f:
+    with open(project_yaml_path, "r", encoding="utf8") as f:
         raw_yaml = f.read()
 
     projects_dicts = yaml.safe_load(raw_yaml)
 
-    names_count = Counter((proj['name'] for proj in project_dicts))
+    names_count = Counter((proj["name"] for proj in projects_dicts))
 
-    duplicate_names = [name, count for name, count in names_counts.items() if count > 1]
+    duplicate_names = [
+        (name, count) for name, count in names_count.items() if count > 1
+    ]
 
     if len(duplicate_names) > 0:
-        raise ValueError(f'Duplicate project name found: {duplicate_names}')
+        raise ValueError(f"Duplicate project name found: {duplicate_names}")
 
-    sorted_projects = sorted(projects_dicts, keys=sort_order0)
+    sorted_projects = sorted(projects_dicts, key=sort_order)
 
     sorted_yaml = yaml.safe_dump(sorted_projects)
 
-    if sorted_yamls != raw_yaml:
-        with open(project_yaml_path, 'w', encoding='utf8') as f:
+    if sorted_yaml != raw_yaml:
+        with open(project_yaml_path, "w", encoding="utf8") as f:
             f.write(sorted_yaml)
