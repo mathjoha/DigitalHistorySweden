6222919f4591e47e70df3f3d1af49fe19a878624
mathjoha <MathiasJoha@gmail.com>
2024-11-24 14:45:11 +0100

    "Add https://portal.research.lu.se/en/projects/need-based-school-funding-a-path-to-promoting-good-working-condit "

--- src/get_url.py
+++ src/get_url.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 from random import shuffle
 from time import sleep
@@ -13,32 +14,39 @@ from get_oru import oru_parse
 from get_su import su_parse
 from get_umu import umu_parse
 from get_uu import uu_parse
-from paths import url_file
+from paths import projects, url_file
 from project_to_yaml import write_project
 
 
 def load_urls():
+    with open(projects, "r", encoding="utf8") as f:
+        proj_txt = f.read()
+    used_urls = set(re.findall("https://\S+\n", proj_txt))
+
     with open(url_file, "r", encoding="utf8") as f:
-        lines = list(set(filter(lambda line: line != "", f.readlines())))
+        lines = list(
+            set(
+                filter(lambda line: line != "" and line not in used_urls, f.readlines())
+            )
+        )
 
     shuffle(lines)
 
     with tqdm(total=len(lines)) as bar:
         while len(lines) > 0:
+            line = lines.pop()
+            yield line.strip()
+
+            with open(url_file, "w", encoding="utf8") as f:
+                f.writelines(sorted(lines))
+            bar.update(1)
             try:
-                line = lines.pop()
-                yield line.strip()
+                first = subprocess.run(["git", "commit", "-am", f'"Format"'])
             finally:
-                with open(url_file, "w", encoding="utf8") as f:
-                    f.writelines(sorted(lines))
-                bar.update(1)
-                try:
-                    first = subprocess.run(["git", "commit", "-am", f'"Format"'])
-                finally:
-                    second = subprocess.check_output(
-                        ["git", "commit", "-am", f'"Add {line}"']
-                    )
-                    assert b"failed" not in second
+                second = subprocess.check_output(
+                    ["git", "commit", "-am", f'"Add {line}"']
+                )
+                assert b"failed" not in second
 
 
 def archive(url):
